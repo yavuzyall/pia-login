28aa0507263c07e9678d809db380207b184b6079
Yavuz Yalçın <yyalcin41@gmail.com>
2024-03-13 16:31:00 +0300

    category names edited

--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -22,7 +22,7 @@ const Accordion = ({ category }: {category: string}) => {
   return (
   <div className="accordion-item">
     <button onClick={toggleAcordion} className="accordion-button w-full text-left px-4 py-2 transition-colors duration-200 ease-in-out hover:bg-blue-700 focus:outline-none">
-        {category}
+        {category.replace(/-/g, ' ').split(' ').map((word) => word.charAt(0).toUpperCase() + word.slice(1)).join(' ')}
     </button>
     <div className={`accordion-content transition-all duration-200 ease-in-out ${isOpen ? 'block' : 'hidden'} `}>
         <ul className="list-none pl-5 space-y-2 ">
